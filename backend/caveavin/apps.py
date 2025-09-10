a98f4f18207c6dfaed7e7aaeac941686deeb7b5b
RonanKer29 <kervella.ronan@hotmail.com>
2025-09-10 11:21:53 +0200

    back

--- backend/caveavin/apps.py
+++ backend/caveavin/apps.py
@@ -1,4 +1,4 @@
-"""Configuration de l'application Cave à Vin.
+"""Configuration de l'application Cave à Vin...
 
 Ce module contient la configuration Django pour l'application caveavin.
 """
@@ -7,10 +7,10 @@ from django.apps import AppConfig
 
 class CaveavinConfig(AppConfig):
     """Configuration de l'application caveavin.
-    
+
     Définit les paramètres de base de l'application Django pour la gestion
     de cave à vin, incluant le type de clé primaire par défaut et le nom de l'app.
-    
+
     Attributes:
         default_auto_field: Type de champ auto-incrémenté par défaut
         name: Nom de l'application Django
